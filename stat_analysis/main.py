61ddf9a240600107b89f8d6314e7f21c74cd3fc8
Tom Gray <tomg77@yahoo.co.uk>
2017-10-16 15:28:43 +0100

    Got a scrollable label working for the logging window

--- stat_analysis/main.py
+++ stat_analysis/main.py
@@ -4,7 +4,9 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.treeview import TreeView,TreeViewLabel
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 class StatAnalysis(Widget):
     pass
@@ -21,6 +23,25 @@ class ActionsScroller(ScrollView):
             tv.add_node(TreeViewLabel(color=(0,0,0,1),text="Sub-item {}".format(i)),n1)
 
         self.add_widget(tv)
+
+
+class HomeView(GridLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text="Home",size_hint=(None,None),height=40,color=(0,0,0,1)))
+        self.add_widget(Button())
+
+
+class LogView(GridLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text="Log",size_hint=(None,None),height=40,color=(0,0,0,1)))
+        self.add_widget(ScrollableLabel(text="Hi\nHi\nHi\nHi\nHi\n\nWorld"))
+
+class ScrollableLabel(ScrollView):
+    text = StringProperty("")
 
 class StatApp(App):
     def build(self):
