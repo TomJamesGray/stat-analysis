83aed5ecbaae43810a6d8322f4196aa7f3b75202
Tom Gray <tomg77@yahoo.co.uk>
2017-10-16 09:48:45 +0100

    Added basic structure for the scrollview widget

--- stat_analysis/main.py
+++ stat_analysis/main.py
@@ -1,14 +1,30 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 from kivy.core.window import Window
 
 class StatAnalysis(Widget):
     pass
 
+class ActionsScroller(ScrollView):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.size_hint = (1,None)
+        self.bar_width = 5
+        layout = GridLayout(cols=1,spacing=10,size_hint_x = 0.3,size_hint_y=None)
+        layout.bind(minimum_height=layout.setter("height"))
+        for i in range(100):
+            btn = Button(text=str(i),size_hint_y=None,height=40)
+            layout.add_widget(btn)
+        self.add_widget(layout)
+
 class StatApp(App):
     def build(self):
         self.title = "Stat Analysis"
         Window.clearcolor = (1,1,1,1)
+        Window.size = (1336,768)
         a = StatAnalysis()
         return a
 
