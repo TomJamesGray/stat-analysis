0d1174d02872c55f106e8ae67b54a3c964651239
Tom Gray <tomg77@yahoo.co.uk>
2017-10-19 14:38:35 +0100

    Added more of the form to regression

--- stat_analysis/actions/stats/regression.py
+++ stat_analysis/actions/stats/regression.py
@@ -14,6 +14,36 @@ class Regression(base_action.BaseAction):
                 "required":True,
                 "form_name":"dataset",
                 "visible_name":"Data Set"
+            },
+            {
+                "input_type": "combo_box",
+                "data_type": "column_numeic",
+                "required": True,
+                "form_name": "x_var",
+                "visible_name": "X Variable"
+            },
+            {
+                "input_type": "combo_box",
+                "data_type": "column_numeic",
+                "required": True,
+                "form_name": "y_var",
+                "visible_name": "Y Variable"
+            },
+            {
+                "input_type": "check_box",
+                "required": True,
+                "form_name": "regression",
+                "visible_name": "Regression"
+            },
+            {
+                "input_type": "text",
+                "default": "1",
+                "data_type": "numeric",
+                "required": "false",
+                "name": "regression_degree",
+                "required_if": "regression=True",
+                "visible_name":"Regression Degree"
+
             }
         ]
         self.output_widget = output_widget
