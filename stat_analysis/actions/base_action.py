eeb4330db9ad07f8d591244cbb69317b58895ef3
Tom Gray <tomg77@yahoo.co.uk>
2017-10-19 11:56:06 +0100

    Added form inputs file to handle the input type in the action form structure

--- stat_analysis/actions/base_action.py
+++ stat_analysis/actions/base_action.py
@@ -1,5 +1,6 @@
 import logging
 from kivy.uix.button import Button
+from stat_analysis.form_inputs import combo_box
 
 logger = logging.getLogger(__name__)
 
@@ -7,4 +8,6 @@ class BaseAction(object):
     def render(self):
         logger.info("Rendering action {}".format(self.type))
         for item in self.form:
-            self.output_widget.add_widget(Button())
+            # self.output_widget.add_widget(Button())
+            self.output_widget.add_widget(combo_box.FormDropDown(label=item["visible_name"],
+                inputs=[{"u_name":"ONE"},{"u_name":"TWO"}]))
---
+++ stat_analysis/form_inputs/combo_box.py
@@ -0,0 +1,20 @@
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+
+class FormDropDown(GridLayout):
+    def __init__(self,label,inputs,*args):
+        super().__init__(*args)
+        self.cols=1
+        self.add_widget(Label(text=label,size_hint=(None,None),color=(0,0,0,1)))
+        self.dropdown = DropDown()
+        for i in inputs:
+            btn = Button(text=i["u_name"],size_hint_y=None,height=30)
+            btn.bind(on_release=lambda btn: self.dropdown.select(btn.text))
+            self.dropdown.add_widget(btn)
+        self.main_btn = Button(text="VALUE",size_hint=(None,None),height=30)
+        self.main_btn.bind(on_release=self.dropdown.open)
+
+        self.dropdown.bind(on_select=lambda instance,x:setattr(self.main_btn,'text',x))
+        self.add_widget(self.main_btn)
