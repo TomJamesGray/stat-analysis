78fd7b7eee44b8a3aed127d4b21420a0cafa969e
Tom Gray <tomg77@yahoo.co.uk>
2017-10-19 14:11:06 +0100

    Using Spinner instead of DropDown

--- stat_analysis/form_inputs/combo_box.py
+++ stat_analysis/form_inputs/combo_box.py
@@ -2,8 +2,11 @@ from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.spinner import Spinner
 # from stat_analysis.main import BorderedButton
 from stat_analysis import main
+
+
 class FormDropDown(GridLayout):
     def __init__(self,label,inputs,*args):
         super().__init__(*args)
@@ -11,15 +14,7 @@ class FormDropDown(GridLayout):
         input_label = Label(text=label,halign="left",size_hint=(1,None),height=20,color=(0,0,0,1))
         input_label.bind(size=input_label.setter("text_size"))
         self.add_widget(input_label)
+        input_vals = tuple([x["u_name"] for x in inputs])
 
-        self.dropdown = DropDown()
-        for i in inputs:
-            btn = Button(text=i["u_name"],size_hint_y=None,height=30)
-            btn.bind(on_release=lambda btn: self.dropdown.select(btn.text))
-            self.dropdown.add_widget(btn)
-        self.main_btn = main.BorderedButton(text="VALUE",size_hint=(None,None),background_color=(1,1,1,1),
-                               background_normal="",color=(0,0,0,1),height=30,b_width=2)
-        self.main_btn.bind(on_release=self.dropdown.open)
-
-        self.dropdown.bind(on_select=lambda instance,x:setattr(self.main_btn,'text',x))
-        self.add_widget(self.main_btn)
+        self.spinner = Spinner(text=label,values=input_vals,size_hint=(None,None))
+        self.add_widget(self.spinner)
