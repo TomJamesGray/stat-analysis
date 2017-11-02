535a518c0acbfd95a29f8f7e6f91f950ab424700
Tom Gray <tomg77@yahoo.co.uk>
2017-11-02 15:08:05 +0000

    Added ability to group inputs together

--- stat_analysis/actions/base_action.py
+++ stat_analysis/actions/base_action.py
@@ -1,24 +1,32 @@
 import logging
-from kivy.uix.button import Button
-from stat_analysis.form_inputs import combo_box,check_box
+from kivy.uix.label import Label
+from stat_analysis.form_inputs import combo_box,check_box,numeric_bounded
 
 logger = logging.getLogger(__name__)
 
 form_input_maps = {
     "combo_box":combo_box.FormDropDown,
-    "check_box":check_box.FormCheckBox
+    "check_box":check_box.FormCheckBox,
+    "numeric_bounded":numeric_bounded.FormNumericBounded
 }
 
 
 class BaseAction(object):
     def render(self):
         logger.info("Rendering action {}".format(self.type))
-        for item in self.form:
-            try:
-                cls = form_input_maps[item["input_type"]]
-            except KeyError:
-                logger.error("Form input {} not found in form_input_maps".format(item["input_type"]))
-                # Go to next form input
-                continue
-            # Give the form widget the whole dict so it can parse the data there
-            self.output_widget.add_widget(cls(item))
+        for group in self.form:
+
+            group_lbl = Label(text=group["group_name"],color=(0,0,0,1),size_hint=(1,None),
+                                                height=30,font_size="22")
+            group_lbl.bind(size=group_lbl.setter("text_size"))
+            self.output_widget.add_widget(group_lbl)
+
+            for item in group["inputs"]:
+                try:
+                    cls = form_input_maps[item["input_type"]]
+                except KeyError:
+                    logger.error("Form input {} not found in form_input_maps".format(item["input_type"]))
+                    # Go to next form input
+                    continue
+                # Give the form widget the whole dict so it can parse the data there
+                self.output_widget.add_widget(cls(item))
--- stat_analysis/actions/stats/regression.py
+++ stat_analysis/actions/stats/regression.py
@@ -8,42 +8,50 @@ class Regression(base_action.BaseAction):
         self.user_name = "XYZ"
         self.status = "OK"
         self.form = [
-            {
-                "input_type":"combo_box",
-                "data_type":"dataset",
-                "required":True,
-                "form_name":"dataset",
-                "visible_name":"Data Set"
-            },
-            {
-                "input_type": "combo_box",
-                "data_type": "column_numeric",
-                "required": True,
-                "form_name": "x_var",
-                "visible_name": "X Variable"
-            },
-            {
-                "input_type": "combo_box",
-                "data_type": "column_numeric",
-                "required": True,
-                "form_name": "y_var",
-                "visible_name": "Y Variable"
-            },
-            {
-                "input_type": "check_box",
-                "required": True,
-                "form_name": "regression",
-                "visible_name": "Regression"
-            },
-            {
-                "input_type": "text",
-                "default": "1",
-                "data_type": "numeric",
-                "required": "false",
-                "name": "regression_degree",
-                "required_if": "regression=True",
-                "visible_name":"Regression Degree"
+            {"group_name":"Data",
+             "inputs":[
+                {
+                    "input_type":"combo_box",
+                    "data_type":"dataset",
+                    "required":True,
+                    "form_name":"dataset",
+                    "visible_name":"Data Set"
+                },
+                {
+                    "input_type": "combo_box",
+                    "data_type": "column_numeric",
+                    "required": True,
+                    "form_name": "x_var",
+                    "visible_name": "X Variable"
+                },
+                {
+                    "input_type": "combo_box",
+                    "data_type": "column_numeric",
+                    "required": True,
+                    "form_name": "y_var",
+                    "visible_name": "Y Variable"
+                }]
+             },
+            {"group_name":"Regression",
+            "inputs":[
+                {
+                    "input_type": "check_box",
+                    "required": True,
+                    "form_name": "regression",
+                    "visible_name": "Regression"
+                },
+                {
+                    "input_type": "numeric_bounded",
+                    "default": 1,
+                    "step":1,
+                    "min":0,
+                    "max":10,
+                    "required": "false",
+                    "name": "regression_degree",
+                    "required_if": "regression=True",
+                    "visible_name":"Regression Degree:"
 
-            }
-        ]
+                }]
+             }
+    ]
         self.output_widget = output_widget
