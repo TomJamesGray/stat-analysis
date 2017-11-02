3736e38c7334963d940f626ed4ffebd000756e7b
TomJamesGray <tomg77@yahoo.co.uk>
2017-11-02 17:45:33 +0000

    Added ScrollView for the action inputs

--- stat_analysis/actions/base_action.py
+++ stat_analysis/actions/base_action.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label
 from kivy.app import App
 from stat_analysis.form_inputs import combo_box,check_box,numeric_bounded
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.splitter import Splitter
 from kivy.uix.button import Button
 
@@ -18,7 +19,8 @@ form_input_maps = {
 class BaseAction(object):
     def render(self):
         logger.info("Rendering action {}".format(self.type))
-        form_layout = GridLayout(cols=1,padding=(5,5),spacing=(10,10),width=220,size_hint=(None,1))
+        form_layout = GridLayout(cols=1,padding=(5,5),spacing=(10,10),width=220,size_hint=(None,None))
+        form_layout.bind(minimum_height=form_layout.setter("height"))
         for group in self.form:
             group_lbl = Label(text=group["group_name"],size_hint=(1,None),
                               height=30,font_size="22",color=App.get_running_app().accent_col)
@@ -35,7 +37,9 @@ class BaseAction(object):
                 # Give the form widget the whole dict so it can parse the data there
                 form_layout.add_widget(cls(item))
 
-        self.output_widget.add_widget(form_layout)
+        scroller = ScrollView(size_hint=(None,1),width=220)
+        scroller.add_widget(form_layout)
+        self.output_widget.add_widget(scroller)
 
         # Create the generic output area
         result_output = ResultOutputWidget(cols=1)
