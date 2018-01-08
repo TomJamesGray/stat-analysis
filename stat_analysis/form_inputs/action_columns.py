9408180e849955e3015490ed2312fc16234a11d0
Tom Gray <tomg77@yahoo.co.uk>
2018-01-08 09:51:56 +0000

    Added custom spinner option to action columns and got the main form panel to scroll if actions columns is too big

--- stat_analysis/form_inputs/action_columns.py
+++ stat_analysis/form_inputs/action_columns.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.spinner import Spinner,SpinnerOption
+# from stat_analysis.generic_widgets.bordered import BorderedButton
 
 logger = logging.getLogger(__name__)
 
@@ -20,8 +21,11 @@ class ActionColumns(GridLayout):
                             font_size="14")
         input_label.bind(size=input_label.setter("text_size"))
         self.add_widget(input_label)
-        self.cols_container = GridLayout(cols=2,row_default_height=30,row_force_default=True)
+        self.cols_container = GridLayout(cols=2,row_default_height=30,row_force_default=True,size_hint_y=None)
+        self.cols_container.bind(minimum_height=self.cols_container.setter("height"))
         self.add_widget(self.cols_container)
+        self.bind(minimum_height=self.setter("height"))
+        # self.height = 400
 
         logger.info("Adding form item to dataset listener group")
         input_dict["add_dataset_listener"](self)
@@ -39,6 +43,15 @@ class ActionColumns(GridLayout):
             for header in dataset.get_headers():
                 self.cols_container.add_widget(Label(text=header,color=(0,0,0,1)))
                 self.cols_container.add_widget(
-                    Spinner(text=self.input_dict["actions"][0],values=self.input_dict["actions"],sync_height=True))
+                    Spinner(text=self.input_dict["actions"][0],values=self.input_dict["actions"],sync_height=True,
+                            option_cls=CustSpinnerOption))
 
             self.prev_dataset_name = dataset_name
+
+
+class CustSpinnerOption(SpinnerOption):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.background_normal = ""
+        self.background_color = (229/255,229/255,229/255,1)
+        self.color = (0,0,0,1)
